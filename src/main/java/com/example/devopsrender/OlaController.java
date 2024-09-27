b34d408af7b97662a364bf6ede19438b735a5d2d
estudosRyan <lucca.ryan.gomes@gmail.com>
2024-09-27 06:33:26 -0300

    corrigindo

--- src/main/java/com/example/devopsrender/OlaController.java
+++ src/main/java/com/example/devopsrender/OlaController.java
@@ -1,12 +1,13 @@
 package com.example.devopsrender;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OlaController {
     @GetMapping("/ola")
-    public String ola() {
-        return "index"; // o Spring irá procurar o arquivo src/main/resources/templates/index.html
+    public ResponseEntity<String> ola() {
+        return ResponseEntity.ok("Davi brito");
     }
 }
